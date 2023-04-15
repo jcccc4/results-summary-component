bea057d46017265e5d08d34f7e1ca26151a63991
jcccc4 <jcsacra4@gmail.com>
2023-04-15 16:00:24 +0800

    Site is already finished but it needs readjustments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,110 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Image from "next/image";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="lg:flex justify-center items-center h-screen w-screen bg-very-light-blue">
+      <div className="lg:h-128 lg:flex lg:w-184 bg-white lg:rounded-custom-box-lg">
+        <div className="bg-gradient-to-b from-violet-a from-0% via-violet-b via-0.01% to-violet-c to-100% h-356  rounded-custom-box flex flex-col items-center lg:h-128 lg:w-3/6  lg:rounded-custom-box-lg">
+          <div className="font-HankenGrotesk-Medium text-lg text-light-blue mt-3.5 lg:text-2xl lg:mt-9.5 lg:mb-8.75">
+            Your Result
+          </div>
+          <div className="bg-gradient-to-b from-circle-a to-circle-b rounded-full h-140 w-140 my-29 text-center flex justify-center items-center flex-col lg:h-50 lg:w-50 lg:mb-6.5">
+            <div className="font-HankenGrotesk-ExtraBold text-56 text-white h-72 lg:text-7xl">
+              76
+            </div>
+            <div className="font-HankenGrotesk-Bold text-base text-light-blue text-opacity-51 lg:text-lg">
+              of 100
+            </div>
+          </div>
+          <div className="text-center w-65 lg:h-33.5 " >
+            <div className="text-2xl text-white font-HankenGrotesk-Medium mb-3 lg:text-8 lg:mb-3.5">
+              Great
+            </div>
+            <div className="text-base text-custom-blue font-HankenGrotesk-Medium text-custom-blue lg:text-lg">
+              Your performance exceed 65% of the people conducting the test
+              here!
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col font-dark-navy gap-3.5 w-full px-8 lg:w-3/6 lg:px-12 lg:text-lg lg:gap-4">
+          <div className="mt-3.5 font-HankenGrotesk-Bold text-lg lg:text-2xl lg:mt-9.5 lg:mb-3">Summary</div>
+          <div className="bg-red-500 bg-opacity-5 flex justify-between items-center h-14 px-4 rounded-xl">
+            <div className="flex w-95 gap-x-3.5 ">
+              <Image
+                src="/images/icon-reaction.svg"
+                alt="Logo"
+                width={20}
+                height={20}
+                priority
+              />
+              <div className=" font-HankenGrotesk-Medium text-base text-custom-red lg:text-lg">
+                Reaction
+              </div>
+            </div>
+            <div className="font-HankenGrotesk-Bold text-dark-navy">
+              <span>80 </span>
+              <span className="opacity-50">/ 100</span>
+            </div>
+          </div>
+          <div className="bg-yellow-500 bg-opacity-5 flex justify-between items-center h-14 px-4 rounded-xl">
+            <div className="flex w-95 gap-x-3 ">
+              <Image
+                src="/images/icon-memory.svg"
+                alt="Logo"
+                width={20}
+                height={20}
+                priority
+              />
+              <div className="font-HankenGrotesk-Medium text-base text-custom-yellow lg:text-lg">
+                Memory
+              </div>
+            </div>
+            <div className="font-HankenGrotesk-Bold text-dark-navy">
+              <span>80 </span>
+              <span className="opacity-50">/ 100</span>
+            </div>
+          </div>
+          <div className="bg-green-500 bg-opacity-5 flex justify-between items-center h-14 px-4 rounded-xl">
+            <div className="flex w-95 gap-x-3">
+              <Image
+                src="/images/icon-verbal.svg"
+                alt="Logo"
+                width={20}
+                height={20}
+                priority
+              />
+              <div className="font-HankenGrotesk-Medium text-base text-custom-green lg:text-lg">
+                Verbal
+              </div>
+            </div>
+            <div className="font-HankenGrotesk-Bold text-dark-navy">
+              <span>80 </span>
+              <span className="opacity-50">/ 100</span>
+            </div>
+          </div>
+          <div className="bg-blue-500 bg-opacity-5 flex justify-between items-center h-14 px-4 rounded-xl">
+            <div className="flex w-95 gap-x-3">
+              <Image
+                src="/images/icon-visual.svg"
+                alt="Logo"
+                width={20}
+                height={20}
+                priority
+              />
+              <div className="font-HankenGrotesk-Medium text-base text-custom-blue-a lg:text-lg">
+                Visual
+              </div>
+            </div>
+            <div className="font-HankenGrotesk-Bold text-dark-navy">
+              <span>80 </span>
+              <span className="opacity-50">/ 100</span>
+            </div>
+          </div>
+          <button className="bg-dark-navy h-14 rounded-full text-white mt-2.5 lg:mt-6.75">
+            Continue
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,85 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "light-blue": "#87CEFA",
+        "violet-a": "#7755FF",
+        "violet-b": "#6943FF",
+        "violet-c": "#2F2CE9",
+        "light-blue": "#CAC9FF",
+        "circle-a": "#4D21C9",
+        "circle-b": "rgba(37, 33, 201, 0)",
+        "custom-blue": "#D9D9D9",
+        "dark-navy": "#303B59",
+        "custom-red":"#FF5555",
+        "custom-yellow":"#FFB21E",
+        "custom-green":"#00BB8F",
+        "custom-blue-a":"#1125D6",
+        "very-light-blue":"#ECF2FF",
       },
+      borderRadius: {
+        "custom-box": "0 0 32px 32px",
+        "custom-box-lg": "32px 32px 32px 32px",
+      },
+      height: {
+        356: "356px",
+        140: "140px",
+        72: "72px",
+        128:"512px",
+        184:"736px",
+        50:"200px",
+        33.5:"134px",
+        15.5:"62px",
+        10.5:"42"
+      },
+      width: {
+        356: "356px",
+        140: "140px",
+        65: "260px",
+        95: "95px",
+        315:"315px",
+        128:"512px",
+        184:"736px",
+        92:"368px",
+        184:"736px",
+        50:"200px"
+      },
+      margin: {
+        29: "29px",
+      },
+      fontFamily: {
+        "HankenGrotesk-Bold": "HankenGrotesk-Bold",
+        "HankenGrotesk-ExtraBold": "HankenGrotesk-ExtraBold",
+        "HankenGrotesk-Medium": "HankenGrotesk-Medium",
+        "HankenGrotesk": "HankenGrotesk",
+      },
+      fontSize: {
+        56: "56px",
+        8:"32px",
+      },
+      textOpacity: {
+        51: ".5168",
+      },
+      textColor:{
+        "custom-blue": "#D9D9D9",
+      },
+      margin:{
+        6.75:"27px",
+        9.5:"38px",
+        8.75:"35px",
+        6.5:"26px"
+      }
+      //...
     },
   },
   plugins: [],
-}
+};
+// 'violet-a': '#7755FF',
+// 'violet-b': '#6943FF',
+// 'violet-c': '#2F2CE9',
