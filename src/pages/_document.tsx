f81e3b54bae834948dd07f6f94cbde000897382e
jcccc4 <jcsacra4@gmail.com>
2023-04-18 18:23:30 +0800

    codemod fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Result Component</title>
-      </Head>
+      <Head />
       <body>
+        <title>Result Component</title>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
