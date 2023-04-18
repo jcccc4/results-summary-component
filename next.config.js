bf350d0c782d92e31a3da640b694c89e4b1f0f39
jcccc4 <jcsacra4@gmail.com>
2023-04-18 15:53:04 +0800

    change api

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+  basePath: '/results-summary-component',
+  assetPrefix: '/results-summary-component/',
 }
 
 module.exports = nextConfig
