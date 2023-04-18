a6e156f95541bac69f7f3ddb8564568929376279
jcccc4 <jcsacra4@gmail.com>
2023-04-18 15:14:12 +0800

    Edit next config js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: '/results-summary-component',
   assetPrefix: '/results-summary-component/',
-  images: {
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    formats: ['image/webp'],
-  },
 }
 
 module.exports = nextConfig
