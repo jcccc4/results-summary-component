27afff89c047239dc0c9f3d0c647d6b0dcdc81a2
jcccc4 <jcsacra4@gmail.com>
2023-04-18 18:05:21 +0800

    fix shadow

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <title>Result Component</title>
         <Main />
         <NextScript />
       </body>
